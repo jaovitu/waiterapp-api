30047cf5fbb1a6f9841ac59a88f9b98a1b2fed86
jaovitu <jvgomes.dev@gmail.com>
2022-12-07 09:36:33 -0300

    feat: adicionando tratamento de erros no método de listagem de categorias

--- src/app/useCases/categories/listCategories.ts
+++ src/app/useCases/categories/listCategories.ts
@@ -3,9 +3,13 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 async function listCategories(request: Request, response: Response) {
-  const categories = await Category.find();
+  try {
+    const categories = await Category.find();
 
-  return response.json(categories);
+    return response.json(categories);
+  } catch(err) {
+    return response.status(500).json({ error: 'Something went wrong.' });
+  }
 }
 
 export { listCategories };
